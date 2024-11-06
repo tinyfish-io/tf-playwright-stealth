d8ef38a265e3863812914aeefaaf2ce183f0f78e
Peter Kirk <lingepumpe@gmail.com>
2024-11-06 08:18:19 +0100

    await page.route when generating stealth headers asynchronously

--- playwright_stealth/stealth.py
+++ playwright_stealth/stealth.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from typing import Union
 from playwright.async_api import Page as AsyncPage
 from playwright.sync_api import Page as SyncPage
 from playwright_stealth.core import StealthConfig
@@ -16,10 +15,17 @@ def combine_scripts(properties: Properties, config: StealthConfig):
         scripts.append(script)
     return "\n".join(scripts)
 
-
-def generate_stealth_headers(properties: Properties, page: Union[AsyncPage, SyncPage]):
+def generate_stealth_headers(properties: Properties, page: SyncPage):
     """Generates the stealth headers for the page by replacing the original headers with the spoofed ones for every request."""
-    page.route("**/*", lambda route: route.continue_(headers=properties.as_dict()["header"]))
+    page.route(
+        "**/*", lambda route: route.continue_(headers=properties.as_dict()["header"])
+    )
+
+async def generate_stealth_headers_async(properties: Properties, page: AsyncPage):
+    """Generates the stealth headers for the page by replacing the original headers with the spoofed ones for every request."""
+    await page.route(
+        "**/*", lambda route: route.continue_(headers=properties.as_dict()["header"])
+    )
 
 
 def stealth_sync(page: SyncPage, config: StealthConfig = None):
@@ -35,6 +41,7 @@ async def stealth_async(page: AsyncPage, config: StealthConfig = None):
     """teaches asynchronous playwright Page to be stealthy like a ninja!"""
     properties = Properties()
     combined_script = combine_scripts(properties, config)
-    generate_stealth_headers(properties, page)
+    await generate_stealth_headers_async(properties, page)
 
     await page.add_init_script(combined_script)
+
